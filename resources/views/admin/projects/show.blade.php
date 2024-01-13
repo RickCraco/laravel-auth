55a90e8156b6469058faf55837803b232a8d21e7
RickCraco <riccardocracolici43@gmail.com>
2024-01-13 01:01:05 +0100

    added style to project show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <section class="container">
-        <h1>{{ $project->title }}</h1>
-        <div class="card w-50">
+    <section class="container my-4">
+        <h1 class="text-danger">{{ $project->title }}</h1>
+        <div class="card w-50 bg-dark text-white border-white">
             <img src="{{ $project->img }}" class="card-img-top" alt="{{ $project->title }}">
             <div class="card-body">
                 <h5 class="card-title">{{ $project->title }}</h5>
                 <p class="card-text">{{ $project->body }}</p>
-                <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-primary">Edit</a>
+                <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-danger">Edit</a>
             </div>
         </div>
     </section>
